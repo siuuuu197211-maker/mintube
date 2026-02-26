23af1a18901613080550bb35566a0c24540b4fc7
민수2 <siuuuu197211@gmail.com>
2026-02-27 08:03:41 +0900

    api변경

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_KEY = 'AIzaSyBao4SOH05ITkhUIDBefDxwhPmt7gCS0pw';  // ← 네 키 그대로
+const API_KEY = 'AIzaSyBymawekE7hmLk9sjD1vjKFtsM08nVD6QY';  // ← 네 키 그대로
 
 async function fetchTrending() {
   try {
